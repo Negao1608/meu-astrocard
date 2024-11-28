2bdb7330d17e83329ae30dc108b97b419cd64801
Negao1608 <00001106384143sp@al.educacao.sp.gov.br>
2024-11-28 08:11:34 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'Qual é a maior estrela do Universo?',
+    'é a VY Canis Majoris'
+)
+
+criaCartao(
+    'Astronomia',
+    'Em qual galáxia nós estamos?',
+    'Via Láctea'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quantos anos tem a Terra?',
+    '4,5 bilhões de anos'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que significa a sigla NASA?',
+    'National Aeronautics and Space Administration'
+)
